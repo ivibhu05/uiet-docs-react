d726684ad615220bf9d7375258804e8bd9346b21
DHEERAJ PANDEY <dp151439@gmail.com>
2022-10-11 00:39:46 +0530

    contact page fixed

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 // import image1 from '../assests/Preeti.png'
 import Mohit from "../../assets/mohits.png";
+import Vibhanshu from "../../assets/vibhu_uiet.jpg";
+import Dheeraj from "../../assets/dp.jpg";
 
 const Contact = () => {
   return (
@@ -12,9 +14,10 @@ const Contact = () => {
           <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
               <img
-                className="  object-cover object-center rounded-circle "
+                className="  object-cover object-center rounded-circle border-5 border-yellow-200 border-solid
+                bg-green-400 w-full h-full "
                 alt="hero"
-                src={Mohit}
+                src={Dheeraj}
               />
             </div>
             <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
@@ -33,15 +36,73 @@ const Contact = () => {
                 truffaut hexagon try-hard chambray.
               </p>
               <div className="flex justify-center">
-                <button
-                  style={{ backgroundColor: "rgb(128,0,0" }}
-                  className="inline-flex text-white  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                <a
+                  href="https://www.linkedin.com/in/dheerajpandey1/"
+                  target="_blank"
                 >
-                  Connect
-                </button>
-                <button className="transition duration-500 ease-in-out bg-grey-500 hover:bg-blue-500 transform hover:-translate-y-1 hover:scale-110 ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                  Button
-                </button>
+                  <button
+                    
+                    style={{ backgroundColor: "rgb(128,0,0" }}
+                    className="inline-flex text-white  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                  >
+                    Connect
+                  </button>
+                </a>
+                <a href="https://dcpandey1.github.io/Portfolio/" target="_blank">
+                  <button className="transition duration-500 ease-in-out bg-green-400 hover:bg-blue-500 transform hover:-translate-y-1 hover:scale-110 ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                    Portfolio
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div>
+        <hr />
+        <section className="text-gray-600 body-font">
+          <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+              <img
+                className="  object-cover object-center rounded-circle border-5 border-yellow-200 border-solid
+                bg-green-400 w-90 h-90 "
+                alt="hero"
+                src={Vibhanshu}
+              />
+            </div>
+            <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Vibhanshu Pandey
+                {/* <br className="hidden lg:inline-block" />readymade gluten */}
+              </h1>
+              <h3 className="title-font sm:text-2xl text-2xl mb-4 font-small text-gray-900">
+                Computer Science & Engineering
+                {/* <br className="hidden lg:inline-block" />readymade gluten */}
+              </h3>
+              <p className="mb-8 leading-relaxed">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p>
+              <div className="flex justify-center">
+              <a
+                  href="https://www.linkedin.com/in/ivibhu05/"
+                  target="_blank"
+                >
+                  <button
+                   
+                    style={{ backgroundColor: "rgb(128,0,0" }}
+                    className="inline-flex text-white  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                  >
+                    Connect
+                  </button>
+                </a>
+                <a href="https://ivibhu05.github.io/Portfolio/" target="_blank">
+                  <button className="transition duration-500 ease-in-out bg-green-400 hover:bg-blue-500 transform hover:-translate-y-1 hover:scale-110 ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                    Portfolio
+                  </button>
+                </a>
               </div>
             </div>
           </div>
@@ -49,41 +110,6 @@ const Contact = () => {
       </div>
 
       <hr />
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-            <img
-              className="object-cover object-center rounded"
-              alt="hero"
-              src="https://dummyimage.com/720x600"
-            />
-          </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Vibhanshu Pandey
-              {/* <br className="hidden lg:inline-block" />readymade gluten */}
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
-            </p>
-
-            <div className="flex justify-center">
-              <button
-                style={{ backgroundColor: "rgb(128,0,0" }}
-                className="inline-flex text-white  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-              >
-                Connect
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
-              </button>
-            </div>
-          </div>
-        </div>
-      </section>
 
       <hr />
       <div>
@@ -94,7 +120,8 @@ const Contact = () => {
                 Contact Us
               </h1>
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                You Can Drop Your Ideas And Feedback Here
+                <b>You Can Drop Your Ideas And Feedback Here</b>
+                
               </p>
             </div>
             <div className="lg:w-1/2 md:w-2/3 mx-auto">
