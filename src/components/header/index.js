95da5d89b502e7a8b574d224bb277d5c396710ce
Vibhanshu Pandey <vibhanshu@getprospa.com>
2022-10-10 16:49:23 +0530

    contribute code factored

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import image from 'src/assests/logo1.png'
-// import Dropdown from './dropdown'
 
 const Header = () => {
   const tabs = [
---
+++ src/utils/contribute-constants/index.js
@@ -0,0 +1,44 @@
+export const CONTRIBUTERS=[
+    {
+        name:'Anit Singh',
+        email:'anitsingh0095@gmail.com',
+        branch:'Computer Science',
+        year:'2019',
+        score:'+50',
+    },
+    {
+        name:'Mohit Rai',
+        email:'anitsingh0095@gmail.com',
+        branch:'Computer Science',
+        year:'2016',
+        score:'+35',
+    },
+    {
+        name:'Akash Madheshiya',
+        email:'anitsingh0095@gmail.com',
+        branch:'Computer Science',
+        year:'2019',
+        score:'+20',
+    },
+    {
+        name:'Hemant Singh',
+        email:'anitsingh0095@gmail.com',
+        branch:'Computer Science',
+        year:'2020',
+        score:'+12',
+    },
+    {
+        name:'Nushoor Khan',
+        email:'anitsingh0095@gmail.com',
+        branch:'Computer Science',
+        year:'2019',
+        score:'+6',
+    },
+    {
+        name:'Agam Jain',
+        email:'anitsingh0095@gmail.com',
+        branch:'Computer Science',
+        year:'2020',
+        score:'+5',
+    },
+]
