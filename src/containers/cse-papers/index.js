6b9dcf02751b4b56300ff36c4e2ebbaaaa451bce
DHEERAJ PANDEY <dp151439@gmail.com>
2022-10-31 16:28:09 +0530

    cse papers added

--- src/containers/cse-papers/index.js
+++ src/containers/cse-papers/index.js
@@ -1,17 +1,147 @@
-import React from 'react'
+import React from "react";
+import { PAPERSCSE } from '../../utils/cspapers';
 
 const CsePapers = () => {
   return (
     <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
           <div className="text-center mb-20">
-            <h1 className="sm:text-4xl text-2xl font-medium  text-center title-font text-gray-900 mb-4">
-              Syllabus
+            <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+              Computer Science & Engineering Papers
             </h1>
             <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-2xl">
-              Get the updated Syllabus for all the departments of all year.
+              Get the latest Papers for Computer Science & Engineering.
             </p>
-         </div>
+          </div>
+          <div className="flex flex-wrap -m-4">
+          {PAPERSCSE?.map(({ link, name }) => (
+            <div className="p-4 lg:w-1/4 sm:w-1/2 w-full">
+              <h2 className="font-medium title-font tracking-widest text-gray-900 mb-4 text-xl text-center sm:text-left">
+                FIRST YEAR
+              </h2>
+              <nav className="flex flex-col sm:items-start sm:text-left text-center items-center -mb-1 space-y-2.5 text-xl">
+                <a href={link} target="_blank" rel="noreferrer">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                      
+                    </svg>
+                    
+                  </span>
+                  {name}
+                </a>
+                <a href="/">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                    </svg>
+                  </span>
+                  Second Link
+                </a>
+                <a href="/">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                    </svg>
+                  </span>
+                  Third Link
+                </a>
+                <a href="/">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                    </svg>
+                  </span>
+                  Fourth Link
+                </a>
+                <a href="/">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                    </svg>
+                  </span>
+                  Fifth Link
+                </a>
+                <a href="/">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                    </svg>
+                  </span>
+                  Fifth Link
+                </a>
+                <a href="/">
+                  <span className="bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={3}
+                      className="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5" />
+                    </svg>
+                  </span>
+                  Fifth Link
+                </a>
+              </nav>
+            </div>
+          ))}
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 };
 export default CsePapers;
---
+++ src/utils/cspapers/index.js
@@ -0,0 +1,28 @@
+export const PAPERSCSE = [
+    {
+      
+      link: 'https://www.codechef.com/UIET2020/',
+      name: 'SUBJECT-CODE',
+      
+    },
+    {
+      
+      link: 'https://www.codechef.com/UIET2020/',
+      name: 'SUBJECT-CODE',
+      
+    },
+    {
+      
+      link: 'https://www.codechef.com/UIET2020/',
+      name: 'SUBJECT-CODE',
+      
+    },
+    {
+      
+      link: 'https://www.codechef.com/UIET2020/',
+      name: 'SUBJECT-CODE',
+      
+    },
+    
+  ];
+  
