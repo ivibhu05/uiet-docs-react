413d756dd8caf1f0608a3858a3ee31990bd909d9
Vibhanshu Pandey <vibhanshu@getprospa.com>
2022-11-12 16:37:11 +0530

    refresh fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,7 +22,7 @@ import ChePapers from '../containers/che-papers';
 const Routes = () => {
   return (
     <>
-      <Router>
+      <Router basename='/uietdocs.com'>
         <Header />
         <Switch>
           <Route exact path="/">
