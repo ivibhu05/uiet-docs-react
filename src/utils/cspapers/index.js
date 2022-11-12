a5331c1ee57ee1d1a25e0872ca777d5e5ec08ef6
Vibhanshu Pandey <vibhanshu@getprospa.com>
2022-11-12 12:10:51 +0530

    cse-papers-done

--- src/utils/cspapers/index.js
+++ src/utils/cspapers/index.js
@@ -1,28 +1,176 @@
-export const PAPERSCSE = [
-    {
-      
-      link: 'https://www.codechef.com/UIET2020/',
-      name: 'SUBJECT-CODE',
-      
-    },
-    {
-      
-      link: 'https://www.codechef.com/UIET2020/',
-      name: 'SUBJECT-CODE',
-      
-    },
-    {
-      
-      link: 'https://www.codechef.com/UIET2020/',
-      name: 'SUBJECT-CODE',
-      
-    },
-    {
-      
-      link: 'https://www.codechef.com/UIET2020/',
-      name: 'SUBJECT-CODE',
-      
-    },
-    
-  ];
-  
+export const PAPERSCSEI = [
+  {
+    link: 'https://drive.google.com/file/d/1spZpLdQALntJJHhrf2YywOAwtQfiKxwq/view',
+    name: 'PHY-S101',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1Wyc6_nij1TqiF1JKA_5_RoHHzP_GRUiw/view',
+    name: 'ESC-S101',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1mAjHfdVu26J9YofJbGwi8fEODfpSOzf6/view',
+    name: 'MTH-S101',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1tqNd76H8WCoahJoSJljNWuOX8MFKVBtb/view',
+    name: 'TCA-S101',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1YNlMzF172nwlyhnj2qTmE1Oovxza7r4S/view',
+    name: 'HSS-S101',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1Nk2sw2AInkH8qpdDh_XtaHea8z__ohLK/view',
+    name: 'PHY-S102',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1umjTkNyRSx8FlnDVsvk1JPIQnMK0Cla-/view',
+    name: 'ISC-S101',
+  },
+
+  {
+    link: 'https://drive.google.com/file/d/1yFgHT5lb5UlRD-5eyu1s_GJ3ZoCXG3Eu/view',
+    name: 'MTH-S102',
+  },
+
+  {
+    link: 'https://drive.google.com/file/d/1Rd7uBOiXokTip10kc_A1vQ7bQwwHrNrw/view',
+    name: 'TCA-S102',
+  },
+  {
+    link: 'https://drive.google.com/file/d/172y1x0sg5-lNF_alK2GDujOMQFzWcrNH/view',
+    name: 'CHM-S101',
+  },
+];
+export const PAPERSCSEII = [
+  {
+    link: 'https://drive.google.com/file/d/1HldjIIMFOuweloEhBBfFwm-bN3c5na-2/view',
+    name: 'MTH-S201',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1qsnGPMAVRb4ClSd4gYYBlkzOrJxHHd8w/view',
+    name: 'ESC-S202',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1NNDiShVd96eAuVhU1EeWZuAa3kvQ9bK5/view',
+    name: 'ESC-S201',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1U8H0h-D0x1Und5X9JfZLZRUut2O4N485/view',
+    name: 'CSE-S201',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1ieHUbaScXI2pq1Aw5CmSoaVPC64Z-l7z/view',
+    name: 'CSE-S202',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1SeXtSiCeocUa4ngN2xdNDPPHRmRJtOna/view',
+    name: 'MTH-S301',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1jXAY4mWrTo3N23R7JKXWC3m1fIQA3WWF/view',
+    name: 'CSE-S204',
+  },
+
+  {
+    link: 'https://drive.google.com/file/d/176F-GKnLTlU0ydLWryZMpBSxrxSGH_9f/view',
+    name: 'CSE-S205',
+  },
+
+  {
+    link: 'https://drive.google.com/file/d/1kmzAc6dvGFZu-orKC_3shxuz9Fbs7nvP/view',
+    name: 'CSE-S206',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1syfTqaGCOqvPQZVIjpqi7e5-6dC08e5A/view',
+    name: 'HSS-S401',
+  },
+];
+export const PAPERSCSEIII = [
+  {
+    link: 'https://drive.google.com/file/d/1GUqRK4hWq3ahdJdGDIxgNAfYbmjMQ4yH/view',
+    name: 'CSE-S301',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1mKA5KvAdRBRlGWi4l2oS9xS94UU_CSL2/view',
+    name: 'CSE-S302',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1mKA5KvAdRBRlGWi4l2oS9xS94UU_CSL2/view',
+    name: 'CSE-S303',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1F8no2z0ICELLyD5Lhd4kSgahzlW7PvJr/view',
+    name: 'CSE-S304',
+  },
+  {
+    link: 'https://drive.google.com/file/d/17SXkzAiwCxN7mZS69aHOfgitxNs4NSKE/view',
+    name: 'MTH-S501',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1HisMD1Dt4v0HPofhE21G34bCxWrmEP6Z/view',
+    name: 'CSE-S305',
+  },
+  {
+    link: 'https://drive.google.com/file/d/1Gv-vy6RIbGk46pXGNAYVWzXc22yR2BFq/view',
+    name: 'CSE-S306',
+  },
+
+  {
+    link: 'https://drive.google.com/file/d/1LsI_K8dpQtkj6OSZ7lWwBf9LZa7QJ24H/view',
+    name: 'CSE-S307',
+  },
+
+  {
+    link: 'https://drive.google.com/file/d/1Ebc-hIof-EQeCQ73pICCwy-ePtqZaxVJ/view',
+    name: 'HSS-S301',
+  },
+  {
+    link: 'https://drive.google.com/file/d/11j04ljyA8aiVpEFzYslHPx2W7ObPiSkT/view',
+    name: 'MTH-S503',
+  },
+];
+export const PAPERSCSEIV = [
+  {
+    link: '',
+    name: 'HSS-S201',
+  },
+  {
+    link: '',
+    name: 'CSE-S401',
+  },
+  {
+    link: '',
+    name: 'SST-S401',
+  },
+  {
+    link: '',
+    name: 'SSM-S401',
+  },
+  {
+    link: '',
+    name: 'PRT-S401',
+  },
+  {
+    link: '',
+    name: 'PRT-S402',
+  },
+  {
+    link: '',
+    name: 'ELECTIVE',
+  },
+
+  {
+    link: '',
+    name: 'ELECTIVE',
+  },
+
+  {
+    link: '',
+    name: 'ELECTIVE',
+  },
+  {
+    link: '',
+    name: 'ELECTIVE',
+  },
+];
