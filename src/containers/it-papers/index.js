c12015020a9c0a85d1d556bfcd437151c015ca08
Vibhanshu Pandey <vibhanshu@getprospa.com>
2022-11-14 11:19:08 +0530

    papers fixed

--- src/containers/it-papers/index.js
+++ src/containers/it-papers/index.js
@@ -3,28 +3,23 @@ import React from 'react';
 const ItPapers = () => {
   return (
     <>
-    
-<div class="bg-yellow-100 relative overflow-hidden h-screen z-0 ">
-    
-    <div class="inset-0 bg-black opacity-25 absolute">
-    </div>
-    <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-        <div class="w-full font-mono flex flex-col items-center relative z-10">
+      <div class="bg-yellow-100 relative overflow-hidden h-screen z-0 ">
+        <div class="inset-0 bg-black opacity-25 absolute"></div>
+        <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+          <div class="w-full font-mono flex flex-col items-center relative z-10">
             <h1 class=" font-extrabold text-2xl text-center text-white  mt-4">
-            Coming Soon... 
-            
-            
+              Coming Soon...
             </h1>
-            <h1 class="font-extrabold text-2xl text-center text-white  mt-4">Send us Paper on Contribute section to help us have
-          more and more resources.</h1>
+            <h1 class="font-extrabold text-2xl text-center text-white  mt-4">
+              Send us Papers on the Contribute section to help us have more and
+              more resources.
+            </h1>
             <p class="font-extrabold text-5xl my-20 text-white animate-bounce">
-                Thankyou
+              Thankyou
             </p>
+          </div>
         </div>
-    </div>
-</div>
-
-      
+      </div>
     </>
   );
 };
