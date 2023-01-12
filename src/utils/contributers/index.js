e22b8d79f93da1233396e5b551295757ea4432d4
Hemant Kumar Singh <shemant820@gmail.com>
2023-01-12 23:12:49 +0530

    contributers laid off

--- src/utils/contributers/index.js
+++ src/utils/contributers/index.js
@@ -1,19 +1,18 @@
 export const CONTRIBUTERS=[
     {
-        name:'Mohit Rai',
-        email:'cenation092@gmail.com',
-        branch:'Computer Science',
-        year:'2016',
-        score:'+35',
-    },
-    {
         name:'Anit Singh',
         email:'anitsingh0095@gmail.com',
         branch:'Computer Science',
         year:'2019',
         score:'+50',
     },
-  
+    {
+        name:'Kashish Kashyap',
+        email:'kashishk0508@gmail.com',
+        branch:'Computer Science',
+        year:'2020',
+        score:'+40',
+    },
     {
         name:'Agam Jain',
         email:'aagamjain573@gmail.com',
@@ -22,21 +21,12 @@ export const CONTRIBUTERS=[
         score:'+40',
     },
     {
-        name:'Kashish Kashyap',
-        email:'kashishk0508@gmail.com',
-        branch:'Computer Science',
-        year:'2020',
-        score:'+30',
-    },
-   
-    {
         name:'Akash Madheshiya',
         email:'akmadhesiya90@gmail',
         branch:'Computer Science',
         year:'2019',
         score:'+20',
-    },
-    
+    },  
     {
         name:'Shobhit Singh',
         email:'imshobhit2003@gmail.com',
--- src/utils/team/index.js
+++ src/utils/team/index.js
@@ -1,6 +1,5 @@
 import Vibhanshu from '../../assets/vibhu_uiet.jpg';
 import Dheeraj from '../../assets/dp2.jpeg';
-import Shivam from '../../assets/shivam_uiet.jpeg'
 import Hemant from '../../assets/hemant.jpeg'
 
 export const TEAM = [
@@ -20,16 +19,6 @@ export const TEAM = [
     portfolio: 'https://ivibhu05.github.io/Portfolio/',
     photo: Vibhanshu,
   },
-
-  
-  {
-    name: 'Shivam Pandey',
-    branch: 'Computer Science & Engineering-2020',
-    role: 'Developer',
-    linkedin: ' https://www.linkedin.com/in/shivam-pandey-77530a202/',
-    portfolio: 'https://starkshiva.me/',
-    photo: Shivam,
-  },
   {
     name: 'Hemant Singh',
     branch: 'Computer Science & Engineering-2020',
@@ -38,6 +27,4 @@ export const TEAM = [
     portfolio: 'https://hemant2742.netlify.app/',
     photo: Hemant,
   },
-  
-
 ];
